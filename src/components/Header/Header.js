3949cdd856d1f69cc2e346141aabe40430a01091
saifursabbir <theesabbir@gmail.com0>
2021-10-18 00:12:02 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="" />
+            {/* <img className="logo" src={logo} alt="" /> */}
             <nav>
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
